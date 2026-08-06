91235d692e17ab4dd3dccbe787b18c6bfd26064d
Rianne Nienke Visscher <nienke.visscher@bsc.es>
2026-08-06 11:55:45 +0200

    measuring variation scripts

--- scripts/3.01_rnv_run_classification.py
+++ scripts/3.01_rnv_run_classification.py
@@ -6,7 +6,8 @@ from datetime import datetime
 project_root = '/home/bsc/bsc093754/GIT/social-media-data-map/'
 
 input_file = f'{project_root}data/processed/inference_sample.csv'
-output_dir = f'{project_root}data/processed/'
+output_dir = f'{project_root}results/01_classification/'
+
 platform = 'Facebook'
 
 print('START ', datetime.now())
